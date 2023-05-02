90ee6ff70098c759d698b9340f514bf7d2519dd1
GaletiHarish <hnani2002n@gmail.com>
2023-05-02 16:03:33 +0530

    Puzzle

--- softskilldetective/softskills/views.py
+++ softskilldetective/softskills/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def homepage(request):
     return render(request, 'softskills/homepage.html')
---
+++ softskilldetective/users/views.py
@@ -0,0 +1,31 @@
+# users/views.py
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('puzzle:index')  # Replace 'puzzle:index' with the URL of your puzzle page
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('puzzle:index')  # Replace 'puzzle:index' with the URL of your puzzle page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('users:login')
